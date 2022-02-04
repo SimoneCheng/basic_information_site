c5c8e9ab2c83821d7a8b4497f4906fb8b4c9249d
SimoneCheng <bleach1827@gmail.com>
2022-02-04 18:54:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/about.html'))
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/contact.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'))
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
